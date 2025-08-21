c51238219782d1580c99bdc0309360223479ab24
Aleksandr Marfin <alexmarfin93@gmail.com>
2025-08-21 19:21:02 +0300

    ADD MainApp

---
+++ src/main/java/com/javarush/marfin/MainApp.java
@@ -0,0 +1,33 @@
+package com.javarush.marfin;
+
+public class MainApp {
+
+    private final static char[] ALPHABET = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
+            'и','к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
+            'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
+
+    public class Chipher {
+        char[] alphabet;
+        public Chipher(char[] alphabet) {
+            this.alphabet =  alphabet;
+        }
+        public String encrypt(String text, int key) {
+
+        }
+
+        public String decrypt(String text, int key) {
+
+        }
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
